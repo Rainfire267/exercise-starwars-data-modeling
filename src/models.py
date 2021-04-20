54068840741ac843f22560087d9d5897bb4deb24
Rainfire267 <ejimenez267@gmail.com>
2021-04-20 22:08:56 +0000

    final version

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Enum, UniqueConstraint
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -11,44 +11,38 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    user_name = Column(String(50))
-    email = Column(String(250), unique=True)
-    password = Column(String(10), nullable=False)
-    loggin = Column(String(20))
-    user = Column(Integer, ForeignKey('user.id'))
-
-class Favorites(Base):
-    __tablename__ = 'favorites'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    user_id = Column(Integer, ForeignKey('person.id'))
-    user = relationship(User)
-
-class Characters(Base):
-    __tablename__ = 'characters'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    gender = Column(String(50))
-    hair_color = Column(String(10))
-    eye_color = Column(String(10))
-    height = Column(Integer)
-    birth_year = Column(String(50))
-    skin_color = Column(String(10))
-    user = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    population = Column(Integer)
-    terrain = Column(String(50))
-    diameter = Column(Integer)
-    climate = Column(String(50))
-    orbital_period = Column(Integer)
-    rotation_period = Column(Integer)
-    user = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    name = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    diameter = Column(Integer, nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    name = Column(String(250), nullable=False)
+    user = relationship("user")
 
     def to_dict(self):
         return {}
