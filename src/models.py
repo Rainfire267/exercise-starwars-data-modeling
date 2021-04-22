bc3d74e33a3d59cd1bab2f268f7c12e22d23c2f8
Rainfire267 <ejimenez267@gmail.com>
2021-04-22 21:10:44 +0000

    corregido relacion con user

--- src/models.py
+++ src/models.py
@@ -8,36 +8,27 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+class Favorites(Base):
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    name = Column(String(250), nullable=False)
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-<<<<<<< HEAD
-    user_name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    password = Column(String(250), nullable=False)
-=======
     user_name = Column(String(50))
     email = Column(String(250), unique=True)
     password = Column(String(10), nullable=False)
     loggin = Column(String(20))
->>>>>>> 9620197a16395b592df3a8338b031cbef342fa06
+    favorites = relationship("Favorites")
 
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-<<<<<<< HEAD
-    name = Column(String(250), nullable=False)
-    terrain = Column(String(250), nullable=False)
-    climate = Column(String(250), nullable=False)
-    population = Column(Integer, nullable=False)
-    orbital_period = Column(Integer, nullable=False)
-    rotation_period = Column(Integer, nullable=False)
-    diameter = Column(Integer, nullable=False)
-=======
     name = Column(String(50))
     user_id = Column(Integer, ForeignKey('person.id'))
     favorites = relationship(User)
->>>>>>> 9620197a16395b592df3a8338b031cbef342fa06
 
 class Characters(Base):
     __tablename__ = 'characters'
@@ -49,13 +40,6 @@ class Characters(Base):
     hair_color = Column(String(250), nullable=False)
     eye_color = Column(String(250), nullable=False)
     skin_color = Column(String(250), nullable=False)
-
-class Favorites(Base):
-    __tablename__ = "favorites"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-    name = Column(String(250), nullable=False)
-    user = relationship("user")
 
     def to_dict(self):
         return {}
